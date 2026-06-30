69670434000599cfc3de0bbebdf0f12d83796b92
mssalvo <salvo.mariniello@gmail.com>
2026-06-30 10:51:08 +0200

    aggiunta comandi

--- app/src/Core/Console/Command.php
+++ app/src/Core/Console/Command.php
@@ -1,5 +1,11 @@
 <?php
 
+/** This file is part of Banquet.
+ * (c) Salvatore Mariniello <salvo.mariniello@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Banquet\Core\Console;
 
 abstract class Command
--- app/src/Core/Console/Commands/MakeActionCommand.php
+++ app/src/Core/Console/Commands/MakeActionCommand.php
@@ -1,5 +1,11 @@
 <?php
 
+/** This file is part of Banquet.
+ * (c) Salvatore Mariniello <salvo.mariniello@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Banquet\Core\Console\Commands;
 
 use Banquet\Core\Console\Command;
@@ -11,69 +17,69 @@ class MakeActionCommand extends Command
 
     public function handle(array $args)
     {
-        $table = $args[0] ?? null;
-
-        if (!$table) {
+        $action = $args[0] ?? null;
+        $service = $args[1] ?? null;
+        if (!$action) {
             echo "❌ Specifica una tabella\n";
             return;
         }
-    if($this->checkEsixt($table)) {
-    echo "✅ Procedo alla creazione della singola action: $table\n";    
-    passthru("php bin/generate --action=$table --with-view --with-route");
-    } else {
-    echo "✅ Procedo alla creazione del flusso completo per action: $table\n";
-        passthru("php bin/generate --table=$table --action=$table --with-view --with-route");
-    }
+        if ($service) {
+        $service= preg_replace('/Service$/', '', $service); 
+        }
+
+        if ($service == null && $this->checkExist($action)) {
+            echo "✅ Procedo alla creazione della singola action: $action\n";
+            passthru("php bin/generate --action=$action --with-service --with-view --with-route");
+        } elseif ($service != null && $this->checkExist($service)) {
+            echo "✅ Procedo alla creazione action: $action con service\n";
+            passthru("php bin/generate --action=$action --action-service=$service --with-service --with-view --with-route");
+        } else {
+            echo "✅ Procedo alla creazione action: $action\n";
+            passthru("php bin/generate --action=$action --with-view --with-route");
+        }
 
     }
 
-private function toPascalCase(string $name)
-{
-    $name = str_replace(" ","", ucwords($name));
-    $name = trim($name);
-    $name = str_replace(['-', '/'], '_', $name);
-    $name = preg_replace('/[^A-Za-z0-9_]+/', ' ', $name);
-    $name = preg_replace('/_+/', '_', $name);
-    $name = ucwords(str_replace('_', ' ', strtolower($name)));
-    $name = str_replace(' ', '', $name);
+    private function toPascalCase(string $name)
+    {
+        $name = str_replace(" ", "", ucwords($name));
+        $name = trim($name);
+        $name = str_replace(['-', '/'], '_', $name);
+        $name = preg_replace('/[^A-Za-z0-9_]+/', ' ', $name);
+        $name = preg_replace('/_+/', '_', $name);
+        $name = ucwords(str_replace('_', ' ', $name));
+        $name = str_replace(' ', '', $name);
 
-    return $name !== '' ? ucfirst($name) : 'Class';
-}
-
-
-private function checkEsixt(string $name){
-$BASE_DIR = dirname(DIR_APP);
-$SRC_DIR  = $BASE_DIR . '/app/src';
-
-$ENTITY_DIR  = $SRC_DIR . '/Entity';
-$DAO_DIR     = $SRC_DIR . '/Dao';
-$MODEL_DIR   = $SRC_DIR . '/Model';
-$SERVICE_DIR = $SRC_DIR . '/Service';
-
-$classe = $this->toPascalCase($name);
-
- 
-$configLoaded = false;
-$configFiles = [
-    $ENTITY_DIR . '/'.$classe.'.php',
-    $SERVICE_DIR . '/'.$classe.'Service.php',
-    $DAO_DIR . '/'.$classe.'Dao.php',
-    $MODEL_DIR . '/'.$classe.'Model.php'
-];
-
- 
-
-foreach ($configFiles as $f) {
-   
-if (file_exists($f)) {
-        $configLoaded = true;
-        
+        return $name !== '' ? ucfirst($name) : 'Class';
     }
-}
 
-return $configLoaded;
 
-}
+    private function checkExist(string $name)
+    {
+        $BASE_DIR = dirname(DIR_APP);
+        $SRC_DIR = $BASE_DIR . '/app/src';
+
+        $SERVICE_DIR = $SRC_DIR . '/Service';
+
+        $classe = $this->toPascalCase($name);
+
+
+        $configLoaded = false;
+        $configFiles = [
+            $SERVICE_DIR . '/' . $classe . 'Service.php'
+        ];
+
+
+
+        if (file_exists($configFiles[0])) {
+            $configLoaded = true;
+
+        }
+
+
+        return $configLoaded;
+
+    }
 
 
 
